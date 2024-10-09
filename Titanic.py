72d752a788d6b66ccda9346825ff250f221d8196
Nthabiseng <nthabisengmokhachane95@gmail.com>
2024-10-09 14:24:07 +0200

    testing this git shit

--- Titanic.py
+++ Titanic.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-titanic_data= pd.read_csv("C:\Users\nthab\OneDrive\Documents\Data science Projects\Titanic survival prediction\train.csv")
-titanic_data.describe()
+
 print(f"hello world")
+print(f"Testing this git shit")
