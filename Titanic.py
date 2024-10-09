714149c0e2b255f389f678afdac6512ea9f135d0
Nthabiseng <nthabisengmokhachane95@gmail.com>
2024-10-09 13:58:04 +0200

    Test

---
+++ Titanic.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+titanic_data= pd.read_csv("C:\Users\nthab\OneDrive\Documents\Data science Projects\Titanic survival prediction\train.csv")
+titanic_data.describe()
+print(f"hello world")
